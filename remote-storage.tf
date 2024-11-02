278d0a62a8889bdff3313cc5a65d84c071535417
bakasifu <156412177+bakanura@users.noreply.github.com>
2024-11-02 20:05:10 +0100

    Update remote-storage.tf

--- remote-storage.tf
+++ remote-storage.tf
@@ -1,30 +1,20 @@
 # Create Storage Account
-resource "azurerm_storage_account" "tfstate" {
-  name                     = "tfstate${random_string.storage_account_name.result}"
-  resource_group_name      = azurerm_resource_group.tfstate.name
-  location                = azurerm_resource_group.tfstate.location
+resource "azurerm_storage_account" "state" {
+  name                     = "tfstate${random_string.suffix.result}"
+  resource_group_name      = azurerm_resource_group.state.name
+  location                 = azurerm_resource_group.state.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
-  
+  min_tls_version         = "TLS1_2"
+
   blob_properties {
     versioning_enabled = true
   }
 }
 
-# Create Container
-resource "azurerm_storage_container" "tfstate" {
+# Create Blob Container
+resource "azurerm_storage_container" "state" {
   name                  = "tfstate"
-  storage_account_name  = azurerm_storage_account.tfstate.name
+  storage_account_name  = azurerm_storage_account.state.name
   container_access_type = "private"
-}
-
-# Output the storage account key and other details needed for backend configuration
-output "storage_account_name" {
-  value = azurerm_storage_account.tfstate.name
-  description = "The name of the storage account"
-}
-
-output "container_name" {
-  value = azurerm_storage_container.tfstate.name
-  description = "The name of the storage container"
 }
