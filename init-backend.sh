f8ff948385a7cc1eec8c6552743f3fbbe3c102a5
bakanura <bakanura@proton.me>
2024-11-04 13:34:55 +0000

    adawd

--- init-backend.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo "Initializing Terraform with Azure backend..."
-terraform init -reconfigure -force-copy -input=false
-echo "Backend initialization complete. Your state is now stored in Azure."
-
-# Remove local state files
-rm -rf terraform.tfstate
-rm -rf terraform.tfstate.backup
