056eea9dffcdf22d92b1018878cda36f4beb34c8
bakanura <bakanura@proton.me>
2024-11-14 13:44:57 +0000

    ad

--- main.tf
+++ main.tf
@@ -23,6 +23,10 @@ terraform {
 }
 
 provider "azurerm" {
+  client_id       = var.ARM_CLIENT_ID
+  client_secret   = var.ARM_CLIENT_SECRET
+  tenant_id       = var.ARM_TENANT_ID
+  subscription_id = var.ARM_SUBSCRIPTION_ID
   features {
 
   }
---
+++ variables.tf
@@ -0,0 +1,19 @@
+variable "ARM_CLIENT_ID" {
+  description = "The client ID for the Azure Service Principal"
+  type        = string
+}
+
+variable "ARM_CLIENT_SECRET" {
+  description = "The client secret for the Azure Service Principal"
+  type        = string
+}
+
+variable "ARM_TENANT_ID" {
+  description = "The tenant ID for the Azure Service Principal"
+  type        = string
+}
+
+variable "ARM_SUBSCRIPTION_ID" {
+  description = "The subscription ID for Azure"
+  type        = string
+}
